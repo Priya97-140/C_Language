5d8be97ad0fc9bae6dba3b5a08c6323ce0e2a01f
Priya97-140 <priyanat67@gmail.com>
2020-03-31 16:25:53 +0530

    Update to code

--- Hello_World.c
+++ Hello_World.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 int main(){
-	printf("Hello World!!!");
+	char input;
+	printf("Hello World!!!\n");
+	printf("Are you able to upload your code to GitHub:(y/n or Y/N)");
+	scanf("%c",&input);
+	if(input=='y'||(input=='Y')){
+		printf("Hello to Git Developers");
+	}
+	else{
+		printf("Sorry Error in committing to GitHub");
+	}
 	return 0;
 }
